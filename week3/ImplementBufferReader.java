876cb55b99f2487086f947ba3fddbb9e3e834430
ANKANA-MONDAL-MCA-A-2024-26-09-UEMK <140162922+ANKANA-MONDAL-MCA-A-2024-26-09-UEMK@users.noreply.github.com>
2025-09-23 08:43:26 +0530

    23/09/2025

--- week3/ImplementBufferReader.java
+++ week3/ImplementBufferReader.java
@@ -1,7 +1,7 @@
 //Write a java program to implement BufferedReader class. 
 import java.io.*;
 
-class ImplementBufferReader {
+class ImplementnBufferReader {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter your name: ");
